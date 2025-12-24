4d901612614eed849fb398004471eafc3b816f7e
muthuviswanath <muthuviswanath@outlook.com>
2025-12-24 15:57:40 +0530

    Day_1_forms

---
+++ html_css_js_xml/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+    public static void main(String[] args){
+        int marks = 54;
+if(marks >= 90){
+    System.out.println("Grade A+");
+    }
+else if (marks >=80) {
+    System.out.println("Grade A");
+}
+}
+}
